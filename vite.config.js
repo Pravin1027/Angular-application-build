ab5f0ca4c3c52d28b84bb7d2073ab499e35b2275
pravin1027 <kakadpravin25@gmail.com>
2026-08-21 10:59:33 +0000

    issue resolve

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
+  // GitHub Pages repository path
+  base: '/react-app/',
+
   build: {
     target: 'es2020',
     outDir: 'dist',
